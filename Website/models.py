10946eed433796e2e06578ab31c41e643c566a26
Jake <jakewolf0825@gmail.com>
2025-05-07 02:19:11 +0800

    Timezone update

--- Website/models.py
+++ Website/models.py
@@ -5,9 +5,10 @@ from datetime import datetime
 import pytz
 
 
-def GetTime():
-    TP_time = datetime.now(pytz.timezone('Asia/Taipei'))
-    return TP_time
+def get_time():
+    # Get the current time in selected timezone
+    get_time_now = datetime.now(pytz.timezone('America/Chicago'))
+    return get_time_now
 
 
 class Note(db.Model):
@@ -15,10 +16,11 @@ class Note(db.Model):
     # ('user.id') is from the User class below except lower case (SQL foreigen key rule)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     note_data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=GetTime)
+    date = db.Column(db.DateTime(timezone=True), default=get_time)
 
+    # Make sure to have the correct timezone below
     def formatted_time(self):
-        return self.date.strftime('%Y-%m-%d %H:%M:%S')
+        return self.date.strftime('%Y-%m-%d %H:%M (CT/UTC-6)')
 
 
 class User(db.Model, UserMixin):
